fb64b4fb1e13460b462f5da10dcb24bc5c35d448
Aaron Derby <aderby@e1z2r7p4.42.us.org>
2019-05-01 16:22:24 -0700

    asfd

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: aderby <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 22:24:36 by aderby            #+#    #+#             */
-/*   Updated: 2019/05/01 13:15:26 by aderby           ###   ########.fr       */
+/*   Updated: 2019/05/01 15:55:13 by aderby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,5 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 		ptr2++;
 		n--;
 	}
-	return (*ptr1 - *ptr2);
+	return (*--ptr1 - *--ptr2);
 }
