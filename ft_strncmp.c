7ca8aeb2138604b6796dbe03ec29df889dccb657
Aaron Derby <aderby@e1z2r7p4.42.us.org>
2019-05-01 16:27:41 -0700

    final push

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -5,27 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aderby <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/29 22:24:36 by aderby            #+#    #+#             */
-/*   Updated: 2019/05/01 15:55:13 by aderby           ###   ########.fr       */
+/*   Created: 2017/06/07 13:49:13 by aderby            #+#    #+#             */
+/*   Updated: 2017/06/13 10:30:35 by aderby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	char	*ptr1;
-	char	*ptr2;
-
-	ptr1 = (char *)s1;
-	ptr2 = (char *)s2;
-	while (*ptr1 && n > 0)
+	while (*s1 && *s1 == *s2 && n > 0)
 	{
-		if (*ptr1 != *ptr2)
-			return (*ptr1 - *ptr2);
-		ptr1++;
-		ptr2++;
+		s1++;
+		s2++;
 		n--;
 	}
-	return (*--ptr1 - *--ptr2);
+	if (n == 0)
+		return (0);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
